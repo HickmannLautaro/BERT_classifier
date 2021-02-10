c23d97718199581fdbe5e16a2e1a72aac7f31304
Lautaro Hickmann <lautyhick@hotmail.com>
2021-02-10 18:01:08 +0100

    Hyperparameter_search setup

--- Hyperparameter_search.py
+++ Hyperparameter_search.py
@@ -1,10 +1,369 @@
 import yaml
 from Train_BERT import run_experiment
+import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
+import os
+
+def hyp_search_lvl1_flatt():
+    HP_MAX_LENGTH = hp.HParam('max_length', hp.Discrete([64, 100, 256, 512]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([60, 20, 40, 5]))
+
+    METRIC_ACCURACY = 'accuracy_score'
+    METRIC_f1 = 'f1_score'
+
+    arguments = {'model_name': 'bert-base-uncased',
+                 'max_length': 100,
+                 'epochs': 40,  #
+                 'batch_size': 40,
+                 'repetitions': 1,
+                 'data_path': 'amazon',
+                 'lvl': 1,
+                 'labels': None,
+                 'test_labels': None,
+                 'hierar': 'flatt',
+                 'lable_type': '_',
+                 'test_labels_type': '_'}
+    model_name = arguments['model_name']
+
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    with tf.summary.create_file_writer("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+            lvl) + '/logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_MAX_LENGTH, HP_BATCH_SIZE],
+            metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy_score'),
+                     hp.Metric(METRIC_f1, display_name='f1_score')],
+        )
+
+    def train_test_model_lvl1_flatt(hparams, arguments):
+        arguments['max_length'] = hparams[HP_MAX_LENGTH]  # arguments['max_length']
+        arguments['batch_size'] = hparams[HP_BATCH_SIZE]  # arguments['epochs']
+
+        f1_score, accuracy_score = run_experiment(arguments, hyp_search=True, )
+
+        return f1_score, accuracy_score
+
+    def run(run_dir, hparams, arguments):
+        with tf.summary.create_file_writer(run_dir).as_default():
+            hp.hparams(hparams)  # record the values used in this trial
+            f1_score, accuracy_score = train_test_model_lvl1_flatt(hparams, arguments)
+            tf.summary.scalar(METRIC_ACCURACY, accuracy_score, step=1)
+            tf.summary.scalar(METRIC_f1, f1_score, step=1)
+
+    session_num = 0
+
+    for max_length in HP_MAX_LENGTH.domain.values[::-1]:
+        for batch_size in HP_BATCH_SIZE.domain.values[::-1]:
+            hparams = {
+                HP_MAX_LENGTH: max_length,
+                HP_BATCH_SIZE: batch_size,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            try:
+                run("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+                    lvl) + '/logs/hparam_tuning/' + run_name, hparams, arguments)
+            except tf.errors.ResourceExhaustedError as e:
+                print("Out of memory")
+
+            session_num += 1
+
+def hyp_search_lvl2_flatt():
+    HP_MAX_LENGTH = hp.HParam('max_length', hp.Discrete([64, 100, 256, 512]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([60, 20, 40, 5]))
+
+    METRIC_ACCURACY = 'accuracy_score'
+    METRIC_f1 = 'f1_score'
+
+    arguments = {'model_name': 'bert-base-uncased',
+                 'max_length': 100,
+                 'epochs': 40,  #
+                 'batch_size': 40,
+                 'repetitions': 1,
+                 'data_path': 'amazon',
+                 'lvl': 2,
+                 'labels': None,
+                 'test_labels': None,
+                 'hierar': 'flatt',
+                 'lable_type': '_',
+                 'test_labels_type': '_'}
+    model_name = arguments['model_name']
+
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    with tf.summary.create_file_writer("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+            lvl) + '/logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_MAX_LENGTH, HP_BATCH_SIZE],
+            metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy_score'),
+                     hp.Metric(METRIC_f1, display_name='f1_score')],
+        )
+
+    def train_test_model_lvl1_flatt(hparams, arguments):
+        arguments['max_length'] = hparams[HP_MAX_LENGTH]  # arguments['max_length']
+        arguments['batch_size'] = hparams[HP_BATCH_SIZE]  # arguments['epochs']
+
+        f1_score, accuracy_score = run_experiment(arguments, hyp_search=True, )
+
+        return f1_score, accuracy_score
+
+    def run(run_dir, hparams, arguments):
+        with tf.summary.create_file_writer(run_dir).as_default():
+            hp.hparams(hparams)  # record the values used in this trial
+            f1_score, accuracy_score = train_test_model_lvl1_flatt(hparams, arguments)
+            tf.summary.scalar(METRIC_ACCURACY, accuracy_score, step=1)
+            tf.summary.scalar(METRIC_f1, f1_score, step=1)
+
+    session_num = 0
+
+    for max_length in HP_MAX_LENGTH.domain.values[::-1]:
+        for batch_size in HP_BATCH_SIZE.domain.values[::-1]:
+            hparams = {
+                HP_MAX_LENGTH: max_length,
+                HP_BATCH_SIZE: batch_size,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            try:
+                run("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+                    lvl) + '/logs/hparam_tuning/' + run_name, hparams, arguments)
+            except tf.errors.ResourceExhaustedError as e:
+                print("Out of memory")
+
+            session_num += 1
+
+def hyp_search_lvl2_target_predicted():
+    HP_MAX_LENGTH = hp.HParam('max_length', hp.Discrete([64, 100, 256, 512]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([60, 20, 40, 5]))
+
+    METRIC_ACCURACY = 'accuracy_score'
+    METRIC_f1 = 'f1_score'
+
+    arguments = {'model_name': 'bert-base-uncased',
+                 'max_length': 100,
+                 'epochs': 40,  #
+                 'batch_size': 40,
+                 'repetitions': 1,
+                 'data_path': 'amazon',
+                 'lvl': 2,
+                 'labels': [['Target', 'Cat1']],
+                 'test_labels': [['saved_data/bert-base-uncased/amazon/lvl1/trained_flatt__tested__/100T_20e/Run4/test_pred_raw.npz']],
+                 'hierar': 'hierarchical',
+                 'lable_type': 'Target',
+                 'test_labels_type': 'Predicted'}
+    model_name = arguments['model_name']
+
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    with tf.summary.create_file_writer("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+            lvl) + '/logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_MAX_LENGTH, HP_BATCH_SIZE],
+            metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy_score'),
+                     hp.Metric(METRIC_f1, display_name='f1_score')],
+        )
+
+    def train_test_model_lvl1_flatt(hparams, arguments):
+        arguments['max_length'] = hparams[HP_MAX_LENGTH]  # arguments['max_length']
+        arguments['batch_size'] = hparams[HP_BATCH_SIZE]  # arguments['epochs']
+
+        f1_score, accuracy_score = run_experiment(arguments, hyp_search=True, )
+
+        return f1_score, accuracy_score
+
+    def run(run_dir, hparams, arguments):
+        with tf.summary.create_file_writer(run_dir).as_default():
+            hp.hparams(hparams)  # record the values used in this trial
+            f1_score, accuracy_score = train_test_model_lvl1_flatt(hparams, arguments)
+            tf.summary.scalar(METRIC_ACCURACY, accuracy_score, step=1)
+            tf.summary.scalar(METRIC_f1, f1_score, step=1)
+
+    session_num = 0
+
+    for max_length in HP_MAX_LENGTH.domain.values[::-1]:
+        for batch_size in HP_BATCH_SIZE.domain.values[::-1]:
+            hparams = {
+                HP_MAX_LENGTH: max_length,
+                HP_BATCH_SIZE: batch_size,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            try:
+                run("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(
+                    lvl) + '/logs/hparam_tuning/' + run_name, hparams, arguments)
+            except tf.errors.ResourceExhaustedError as e:
+                print("Out of memory")
+
+            session_num += 1
+
+def hyp_search_lvl2_target_target():
+    HP_MAX_LENGTH = hp.HParam('max_length', hp.Discrete([64, 100, 256, 512]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([60, 20, 40, 5]))
+
+    METRIC_ACCURACY = 'accuracy_score'
+    METRIC_f1 = 'f1_score'
+
+    arguments = {'model_name': 'bert-base-uncased',
+                 'max_length': 100,
+                 'epochs': 40,  #
+                 'batch_size': 40,
+                 'repetitions': 1,
+                 'data_path': 'amazon',
+                 'lvl': 2,
+                 'labels': [['Target','Cat1']],
+                 'test_labels': [['Target','Cat1']],
+                 'hierar': 'hierarchical',
+                 'lable_type': 'Target',
+                 'test_labels_type': 'Target'}
+    model_name = arguments['model_name']
+
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    with tf.summary.create_file_writer("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(lvl) + '/logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_MAX_LENGTH, HP_BATCH_SIZE],
+            metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy_score'),
+                     hp.Metric(METRIC_f1, display_name='f1_score')],
+        )
 
 
+    def train_test_model_lvl1_flatt(hparams, arguments):
+        arguments['max_length'] = hparams[HP_MAX_LENGTH]  # arguments['max_length']
+        arguments['batch_size'] = hparams[HP_BATCH_SIZE]  # arguments['epochs']
 
-conf = "./Configs/amazon_config_lvl1_bert-base-uncased.yaml"
-with open(conf) as f:
-    arguments = yaml.load(f, Loader=yaml.FullLoader)
-for i in range(arguments['repetitions']):
-    run_experiment(arguments, True)
+        f1_score, accuracy_score = run_experiment(arguments, hyp_search=True, )
+
+        return f1_score, accuracy_score
+
+    def run(run_dir, hparams, arguments):
+        with tf.summary.create_file_writer(run_dir).as_default():
+            hp.hparams(hparams)  # record the values used in this trial
+            f1_score, accuracy_score = train_test_model_lvl1_flatt(hparams, arguments)
+            tf.summary.scalar(METRIC_ACCURACY, accuracy_score, step=1)
+            tf.summary.scalar(METRIC_f1, f1_score, step=1)
+
+    session_num = 0
+
+    for max_length in HP_MAX_LENGTH.domain.values[::-1]:
+        for batch_size in HP_BATCH_SIZE.domain.values[::-1]:
+            hparams = {
+                HP_MAX_LENGTH: max_length,
+                HP_BATCH_SIZE: batch_size,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            try:
+                run("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str( lvl) + '/logs/hparam_tuning/' + run_name, hparams, arguments)
+            except tf.errors.ResourceExhaustedError as e:
+                print("Out of memory")
+
+            session_num += 1
+
+def hyp_search_lvl2_predicted_predicted():
+    HP_MAX_LENGTH = hp.HParam('max_length', hp.Discrete([64, 100, 256, 512]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([60, 20, 40, 5]))
+
+    METRIC_ACCURACY = 'accuracy_score'
+    METRIC_f1 = 'f1_score'
+
+    arguments = {'model_name': 'bert-base-uncased',
+                 'max_length': 100,
+                 'epochs': 40,  #
+                 'batch_size': 40,
+                 'repetitions': 1,
+                 'data_path': 'amazon',
+                 'lvl': 2,
+                 'labels': [['saved_data/bert-base-uncased/amazon/lvl1/trained_flatt__tested__/100T_20e/Run4/test_pred_raw.npz']],
+                 'test_labels': [['saved_data/bert-base-uncased/amazon/lvl1/trained_flatt__tested__/100T_20e/Run4/test_pred_raw.npz']],
+                 'hierar': 'hierarchical',
+                 'lable_type': 'Predicted',
+                 'test_labels_type': 'Predicted'}
+    model_name = arguments['model_name']
+
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    with tf.summary.create_file_writer("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str(lvl) + '/logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_MAX_LENGTH, HP_BATCH_SIZE],
+            metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy_score'),
+                     hp.Metric(METRIC_f1, display_name='f1_score')],
+        )
+
+
+    def train_test_model_lvl1_flatt(hparams, arguments):
+        arguments['max_length'] = hparams[HP_MAX_LENGTH]  # arguments['max_length']
+        arguments['batch_size'] = hparams[HP_BATCH_SIZE]  # arguments['epochs']
+
+        f1_score, accuracy_score = run_experiment(arguments, hyp_search=True, )
+
+        return f1_score, accuracy_score
+
+    def run(run_dir, hparams, arguments):
+        with tf.summary.create_file_writer(run_dir).as_default():
+            hp.hparams(hparams)  # record the values used in this trial
+            f1_score, accuracy_score = train_test_model_lvl1_flatt(hparams, arguments)
+            tf.summary.scalar(METRIC_ACCURACY, accuracy_score, step=1)
+            tf.summary.scalar(METRIC_f1, f1_score, step=1)
+
+    session_num = 0
+
+    for max_length in HP_MAX_LENGTH.domain.values[::-1]:
+        for batch_size in HP_BATCH_SIZE.domain.values[::-1]:
+            hparams = {
+                HP_MAX_LENGTH: max_length,
+                HP_BATCH_SIZE: batch_size,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            try:
+                run("hyperparameters_search/" + model_name + "/" + data_path + "/lvl" + str( lvl) + '/logs/hparam_tuning/' + run_name, hparams, arguments)
+            except tf.errors.ResourceExhaustedError as e:
+                print("Out of memory")
+
+            session_num += 1
+
+def main():
+    print("Tensorflow version: ", tf.__version__)
+
+    # rtx 3080 tf 2.4.0-rc4 bug
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    hyp_search_lvl1_flatt()
+    print("hyp_search_lvl1_flatt done")
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    hyp_search_lvl2_flatt()
+    print("hyp_search_lvl2_flatt done")
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    hyp_search_lvl2_target_predicted()
+    print("hyp_search_lvl2_target_predicted done")
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    hyp_search_lvl2_target_target()
+    print("hyp_search_lvl2_target_target done")
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    print("#" * 150)
+    hyp_search_lvl2_predicted_predicted()
+if __name__ == "__main__":
+    main()
--- Train_BERT.py
+++ Train_BERT.py
@@ -21,11 +21,43 @@ import tensorflow_addons as tfa
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
-import itertools
 import io
+import itertools
 import os
 import yaml
 import sys
+
+
+def plot_confusion_matrix(cm, f1_score, accuracy_score, class_names):
+    """
+    Returns a matplotlib figure containing the plotted confusion matrix.
+    Args:
+        :param cm: (array, shape = [n, n]): a confusion matrix of integer classes
+        :param class_names: (array, shape = [n]): String names of the integer classes
+        :param accuracy_score: accuracy score for plotting
+        :param f1_score: f1_score score for plotting
+    """
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title("Confusion matrix, accuracy {:.4f} \n f1 Score {:.4f}".format(accuracy_score, f1_score))
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    # Compute the labels from the normalized confusion matrix.
+    labels = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+    # Use white text if squares are dark; otherwise black.
+    threshold = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, labels[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return figure
 
 
 def append_label(data, arguments):
@@ -50,7 +82,24 @@ def append_label(data, arguments):
     return data
 
 
-def get_data(arguments):
+def append_test_label(test, arguments):
+    if arguments['test_labels'] is not None:
+        for arg in arguments['test_labels']:
+            if arg[0] == 'Target':
+                for cat in arg[1:][::-1]:
+                    test['Text'] = test[cat].str.cat(test['Text'], sep=". ")
+                    # test['Text']=test[cat].str.cat(test['Text'],sep=". ")
+            else:
+                for cat in arg[::-1]:
+                    file = np.load(cat, allow_pickle=True)
+                    labels_test = file['test_class_names'][file['test_pred_raw'].argmax(axis=1)]
+                    test['aux'] = labels_test
+                    test['Text'] = test['aux'].str.cat(test['Text'], sep=". ")
+
+    return test
+
+
+def get_data(arguments,hyp_search=False):
     lvl = arguments['lvl']
     data_path = arguments['data_path']
 
@@ -74,17 +123,47 @@ def get_data(arguments):
     data[cat_num] = data[cat_label].cat.codes
 
     train_class_names = np.unique(data[cat_label])
-
-    print("Class names: \n Train: {} ".format(train_class_names))
-    print("Training data")
-    print(data.head())
+    if not hyp_search:
+        print("Class names: \n Train: {} ".format(train_class_names))
+        print("Training data")
+        print(data.head())
 
     train_target = to_categorical(data[cat_num])
 
     return data, train_class_names, train_target
 
 
-def get_bert_model(model_name, config, max_length, class_names):
+def get_test_data(arguments):
+    lvl = arguments['lvl']
+    data_path = arguments['data_path']
+
+    # Import data from csv
+    test = pd.read_csv(data_path + "/test.csv")
+    test = test.rename(columns={"text": "Text", "l1": "Cat1", "l2": "Cat2", "l3": "Cat3"})
+
+    # Add labels to text
+    test = append_test_label(test, arguments)
+
+    # Select target columns
+    cat_num = str('Cat' + str(lvl))
+
+    test = test[['Text', cat_num]]
+
+    # Setup test data for logging
+    cat_label = str(cat_num + '_label')
+    # Set model output as categorical and save in new label col
+    test[cat_label] = pd.Categorical(test[cat_num])
+    # Transform your output to numeric
+    test[cat_num] = test[cat_label].cat.codes
+
+    test_class_names = np.unique(test[cat_label])
+
+    test_target = test[cat_num]
+
+    return test, test_class_names, test_target
+
+
+def get_bert_model(model_name, config, max_length, class_names, hyp_search):
     # Load the Transformers BERT model
     transformer_model = TFBertModel.from_pretrained(model_name, config=config)
 
@@ -113,8 +192,9 @@ def get_bert_model(model_name, config, max_length, class_names):
     # And combine it all in a model object
     model = Model(inputs=inputs, outputs=output, name='BERT_MultiClass')
 
-    # Take a look at the model
-    print(model.summary())
+    if not hyp_search:
+        # Take a look at the model
+        print(model.summary())
 
     # ------- Setup training ------- ###
 
@@ -159,15 +239,9 @@ def get_tokenized(model_name, config, data, max_length):
 
 
 def run_experiment(arguments, hyp_search=False):
-    print("Tensorflow version: ", tf.__version__)
-
-    # rtx 3080 tf 2.4.0-rc4 bug
-    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-    print("#" * 150)
-    print("#" * 150)
+    if not hyp_search:
+        print("#" * 150)
+        print("#" * 150)
 
     # --------- Setup BERT ----------
     # Name of the BERT model to use
@@ -186,11 +260,11 @@ def run_experiment(arguments, hyp_search=False):
         lvl) + "/trained_" + hierar + "_" + lable_type + "tested_" + test_labels_type + "/" + str(
         max_length) + "T_" + str(
         epochs) + "e/"
+
     if hyp_search:
         aux_path = os.getcwd() + "/hyperparameters_search" + path
     else:
         aux_path = os.getcwd() + "/saved_models" + path
-
 
     try:
         os.makedirs(aux_path)
@@ -219,6 +293,13 @@ def run_experiment(arguments, hyp_search=False):
     else:
         path_model = "./saved_models" + path
 
+    try:
+        os.makedirs(path_model)
+    except OSError:
+        print("%s already exists" % path_model)
+    else:
+        print("Successfully created the directory %s " % path_model)
+
     logdir = path_model + "/logs"
     path_save_model = path_model + "/model/"
 
@@ -233,7 +314,7 @@ def run_experiment(arguments, hyp_search=False):
 
     ### --------- Import data --------- ###
 
-    data, train_class_names, target = get_data(arguments)
+    data, train_class_names, target = get_data(arguments, hyp_search)
     ### --------- Load BERT ---------- ###
 
     # Load transformers config and set output_hidden_states to False
@@ -242,7 +323,7 @@ def run_experiment(arguments, hyp_search=False):
 
     # Load the Transformers BERT model
 
-    model = get_bert_model(model_name, config, max_length, train_class_names)
+    model = get_bert_model(model_name, config, max_length, train_class_names, hyp_search)
 
     # Tokenize the input (takes some time) for training and test (for logging) data
 
@@ -252,7 +333,9 @@ def run_experiment(arguments, hyp_search=False):
     # Tensorboard callback
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, write_graph=False,
                                                           write_images=True, profile_batch='10,20')
-    earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_f1_score', verbose=3, mode="max", restore_best_weights=True)
+
+    earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_f1_score', verbose=1, mode="max",patience=4,
+                                                     restore_best_weights=True, min_delta=0.0001)
     ### ------- Train the model ------- ###
     # Batch size table rtx 3080
     # 100T base-uncased: 50
@@ -265,10 +348,11 @@ def run_experiment(arguments, hyp_search=False):
         validation_split=0.2,
         batch_size=batch_size,
         epochs=epochs,
-        callbacks=[tensorboard_callback,earlystopping])
+        callbacks=[tensorboard_callback, earlystopping])
 
     # Load the best weights and save the model
-    model.save(path_save_model)
+    if not hyp_search:
+        model.save(path_save_model)
 
     tf.keras.utils.plot_model(
         model,
@@ -281,9 +365,44 @@ def run_experiment(arguments, hyp_search=False):
     )
 
     print("Run finished: " + path)
+    if hyp_search:
+        test, test_class_names, test_target = get_test_data(arguments)
+        # ----- Evaluate the model ------
+        test_x = get_tokenized(model_name, config, test, max_length)
+
+        test_pred_raw = model.predict(x={'input_ids': test_x['input_ids'], 'attention_mask': test_x['attention_mask']},
+                                      verbose=1)
+
+        test_pred = np.argmax(test_pred_raw, axis=1)
+        # Calculate the confusion matrix.
+        cm = sklearn.metrics.confusion_matrix(test_target, test_pred)
+        cm[np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2) < 0.05] = 0
+
+        f1_score = sklearn.metrics.f1_score(test_target, test_pred, average='macro')
+        accuracy_score = sklearn.metrics.accuracy_score(test_target, test_pred)
+
+        path_confusion_mat = path_model + '/conf.png'
+
+        figure = plot_confusion_matrix(cm, f1_score, accuracy_score, class_names=test_class_names)
+        figure.savefig(path_confusion_mat)
+        plt.close(figure)
+
+        # noinspection PyTypeChecker
+        report = sklearn.metrics.classification_report(test_target, test_pred, target_names=test_class_names, digits=4)
+
+        np.savez(path_model+ '/rep_amd_histo.npz', report=report, hist=history.history)
+
+        return f1_score, accuracy_score
 
 
 def main():
+    print("Tensorflow version: ", tf.__version__)
+
+    # rtx 3080 tf 2.4.0-rc4 bug
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
     list_args = sys.argv[1:]
 
     if len(list_args) < 1:
