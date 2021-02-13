8d808353ae5b9e69d12861832f41357615b56dd4
Lautaro Hickmann <lautyhick@hotmail.com>
2021-02-13 16:09:30 +0100

    Added batch to saved model path

--- Train_BERT.py
+++ Train_BERT.py
@@ -260,7 +260,7 @@ def run_experiment(arguments, hyp_search=False):
     path = "/" + model_name + "/" + data_path + "/lvl" + str(
         lvl) + "/trained_" + hierar + "_" + lable_type + "/" + str(
         max_length) + "T_" + str(
-        epochs) + "e/"
+        epochs) + "e_" + str(batch_size) + "b/"
 
     if hyp_search:
         aux_path = os.getcwd() + "/hyperparameters_search" + path
